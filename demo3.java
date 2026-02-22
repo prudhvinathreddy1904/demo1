d9902354ac831cd681369ff7c8d13c8c7ee7dab9
prudhvinathreddy1904 <mprudhvinathreddy7777@gmail.com>
2026-02-22 14:30:57 +0530

    Update demo3.java

--- demo3.java
+++ demo3.java
@@ -2,13 +2,13 @@ public class Demo2 {
 
     public static void main(String[] args) {
 
-        System.out.println(String.join(" ",  "***", "    ***", "  *****", "  *****"));
-        System.out.println(String.join(" ", "*   *", " *   *", " *    *", " *    *"));
-        System.out.println(String.join(" ", "*   *", " *   *", " *    *", " *    *"));
-        System.out.println(String.join(" ", "*   *", " *   *", " *****", "  *****"));
-        System.out.println(String.join(" ", "*   *", " *   *", " *", "      *"));
-        System.out.println(String.join(" ", "*   *", " *   *", " *", "      *"));
-        System.out.println(String.join(" ",  "***", "    ***",   " *", "      *"));
+        System.out.println(String.join(" ",  "***", "    ***", "   *****", "  *****"));
+        System.out.println(String.join(" ", "*   *", " *     *", " *    *", " *    "));
+        System.out.println(String.join(" ", "*   *", " *     *", " *    *", " *    "));
+        System.out.println(String.join(" ", "*   *", " *     *", " *****", "  *****"));
+        System.out.println(String.join(" ", "*   *", " *     *", " *", "           *"));
+        System.out.println(String.join(" ", "*   *", " *     *", " *", "           *"))  ;
+        System.out.println(String.join(" ",  "***", "    ***",   " *", "      *****"));
 
     }
 }
